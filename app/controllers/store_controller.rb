17e053b8a2cf9fda1b0a0c1c28de6b6e602db68f
Ronald Suez <rsuez93@gmail.com>
2015-06-01 12:31:34 -0430

    added preliminar solution

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -9,15 +9,42 @@ class StoreController < ApplicationController
      @query = params[:q]
      
 
+     @query_strings = params[:q].scan(/\w+/)
 
      result = []
-     clothing_types = ClothingType.all
-     clothing_types.each do |clothing_type|
-     	  result << clothing_type.name
-     end
 
+     puts @query_strings
+
+     clothing_types = ClothingType.all
+     brands = Brand.all
+
+     @query_strings.each do |query|
+        flag = false
+
+        clothing_types.each do  |clothing_type|
+          match = clothing_type.name.scan(/#{query}/)
+          if (match.any?)
+            puts match.to_s
+            result << ("<i>" + query + "</i>").html_safe
+            flag = true
+          end
+        end
+
+       brands.each do |brand|
+            match = brand.name.scan(/#{query}/)
+            if (match.any?)
+              puts match.to_s
+              result << ("<b>" + query + "</b>").html_safe
+              flag = true
+           end
+       end
+
+      if(!flag)
+        result << query
+      end
+       puts '------'
+     end
      @results = result
-     #brands = Brand.all
 
   end
 end
